2f46e5bc1489aa19cdef3de0f3597faa8e731a44
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2020-04-19 14:25:42 +0200

    +initial mockup separating front and amtriz generation

---
+++ tasks.js
@@ -0,0 +1,64 @@
+const tasks =
+ [
+    {
+        id: 0,
+        name: 'run_partition_simulation_drug_1',
+        subject: 'practices',
+        startDate: new Date(2020, 3, 28),
+        endDate: new Date(2020, 3, 29) 
+    },
+    {
+        id: 1,
+        name: 'qm_force_field_vaccuum',
+        subject: 'masters-thesis',
+        startDate: new Date(2020, 3, 30),
+        endDate: new Date(2020, 3, 31) 
+    },
+    {
+        id: 2,
+        name: 'qm_force_field_water',
+        subject: 'masters-thesis',
+        startDate: new Date(2020, 4, 1),
+        endDate: new Date(2020, 4, 5) 
+    },
+    {
+        id: 3,
+        name: 'run_partition_simulation_drug_2',
+        subject: 'practices',
+        startDate: new Date(2020, 3, 30),
+        endDate: new Date(2020, 4, 3) 
+    },
+    {
+        id: 4,
+        name: 'run_partition_simulation_drug_3',
+        subject: 'practices',
+        startDate: new Date(2020, 4, 6),
+        endDate: new Date(2020, 4, 10) 
+    },
+    {
+        id: 5,
+        name: 'write_masters_thesis_report',
+        subject: 'masters-thesis',
+        startDate: new Date(2020, 4, 11),
+        endDate: new Date(2020, 4, 12) 
+    },
+    {
+        id: 6,
+        name: 'biological_force_field_complete_in_report',
+        subject: 'masters-thesis',
+        startDate: new Date(2020, 4, 13),
+        endDate: new Date(2020, 4, 26) 
+    },
+    {
+        id: 7,
+        name: 'qm_force_field_vaccuum',
+        subject: 'masters-thesis',
+        startDate: new Date(2020, 4, 27),
+        endDate: new Date(2020, 5, 10)
+    }
+]
+
+const subject_colors = {
+    "practices": "#8C9440",
+    "masters-thesis": "#85678F"
+}
