b915d4e5a043c0f7364c416e633a6cd233d600c6
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2020-04-19 19:57:36 +0200

    # implementing matrix random filling but with limited displays

--- back-end/run_simulation.js
+++ back-end/run_simulation.js
@@ -1,16 +1,30 @@
 function generate_matrix(rows_size, cols_size = rows_size) {
-    const htmlToFillInCells = '<td style=\
-    "background-color:#f0c674;width:5px;height:5px"\
+    const prevHtmlToFillInCells = '<td ';
+    const postHtmlToFillInCells = 'style=\
+    "width:120px;height:120px;border:2px solid #282a2e;"\
     ></td>';
     let htmlToFillInMatrices = '<td align="center" valign="middle">';
     htmlToFillInMatrices += '<table id="matrix_table">';
-    for (i = 0; i < rows_size; i++) {
-        htmlToFillInMatrices += '<tr style="">';
-        for (j = 0; j < cols_size; j++) {
-            htmlToFillInMatrices += '<td>' + htmlToFillInCells + '</td>';
+    let n_elements = 3;
+    let counter = 0;
+    let htmlToFillColorsInMatrices;
+    while (counter < n_elements) {
+        htmlToFillColorsInMatrices = "";
+        for (i = 0; i < rows_size; i++) {
+            htmlToFillColorsInMatrices += '<tr>';
+            for (j = 0; j < cols_size; j++) {
+                if (Math.random() > 0.88888) {
+                    htmlToFillColorsInMatrices += prevHtmlToFillInCells + 'class="blue" ' + postHtmlToFillInCells;
+                    counter += 1
+                } else {
+                    htmlToFillColorsInMatrices += prevHtmlToFillInCells + postHtmlToFillInCells;
+                }
+            }
+            htmlToFillColorsInMatrices += '</tr>';
         }
-        htmlToFillInMatrices += '</tr>'
     }
+
+    htmlToFillInMatrices += htmlToFillColorsInMatrices;
     htmlToFillInMatrices += '</table>';
     htmlToFillInMatrices += '</td>';
     document.getElementById('exercise_placeholder').innerHTML = htmlToFillInMatrices;
@@ -21,10 +35,10 @@ function runSimulation() {
     const element_selection = document.getElementById("element_selection").value;
     const matrix_size = parseInt(document.getElementById("matrix_size").innerText.substring(0, 1));
     const elements_number = parseInt(document.getElementById("elements_number").innerText);
-    console.log("Exercise selection: " + exercise_selection);
-    console.log("Element selection: " + element_selection);
-    console.log("matrix_size selection: " + matrix_size);
-    console.log("elements_number selection: " + elements_number);
+    // console.log("Exercise selection: " + exercise_selection);
+    // console.log("Element selection: " + element_selection);
+    // console.log("matrix_size selection: " + matrix_size);
+    // console.log("elements_number selection: " + elements_number);
     if (exercise_selection === "Memoria rápida") {
         if (element_selection === "Matrices") {
             generate_matrix(matrix_size);
