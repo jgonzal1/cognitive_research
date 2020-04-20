9ffa00f63e9196af1c1eb4e9751668cf353679ef
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2020-04-20 15:14:47 +0200

    + number arithmetics

--- back-end/run_simulation.js
+++ back-end/run_simulation.js
@@ -2,11 +2,15 @@ function fill_matrix(rows_size, cols_size = rows_size, callback) {
     let counter = 0;
     let htmlToFillColorsInMatrices;
     const prevHtmlToFillInCells = '<td ';
-    const postHtmlToFillInCells = 'style=\
-    "width:120px;height:120px;border:2px solid #282a2e;"\
-    ></td>';
-    const elements_number = parseInt(document.getElementById("elements_number").innerText);
+    const element_selection = document.getElementById("element_selection").value;
     const matrix_size = parseInt(document.getElementById("matrix_size").innerText.substring(0, 1));
+    const elements_number = parseInt(document.getElementById("elements_number").innerText);
+    let postHtmlToFillInCells;
+    if (element_selection === "Matrices") {
+        postHtmlToFillInCells = 'style=\
+        "width:120px;height:120px;border:2px solid #282a2e;"\
+        ></td>';
+    }
     const p_fill = (1 - (elements_number / 18 /*(matrix_size ^ 2) * 2*/ ));
     console.log(matrix_size + " " + p_fill)
     while (counter < elements_number) {
@@ -16,9 +20,30 @@ function fill_matrix(rows_size, cols_size = rows_size, callback) {
             htmlToFillColorsInMatrices += '<tr>';
             for (j = 0; j < cols_size; j++) {
                 if (Math.random() > p_fill) {
-                    htmlToFillColorsInMatrices += prevHtmlToFillInCells + 'class="blue" ' + postHtmlToFillInCells;
+                    if (element_selection === "Matrices") {
+                        htmlToFillColorsInMatrices += prevHtmlToFillInCells + 'class="blue"' + postHtmlToFillInCells;
+                    } else if (element_selection === "Números") {
+                        let color;
+                        if (Math.random() < 0.5) {
+                            color = "#A54242"
+                        } else {
+                            color = "#5E8D87"
+                        }
+                        postHtmlToFillInCells = 'style=\
+                        "color:' + color + ';font-size:100px;font-weight:bolder;width:120px;height:120px;"\
+                        >' + Math.floor(10 * Math.random()) + '</td>';
+                        htmlToFillColorsInMatrices += prevHtmlToFillInCells + postHtmlToFillInCells;
+                    }
                     counter += 1
                 } else {
+                    if (element_selection === "Matrices") {
+                        postHtmlToFillInCells = 'style=\
+                            "width:120px;height:120px;border:2px solid #282a2e;"\
+                            ></td>';
+                    } else {
+                        postHtmlToFillInCells = 'style=\
+                            "width:120px;height:120px;"></td>';
+                    }
                     htmlToFillColorsInMatrices += prevHtmlToFillInCells + postHtmlToFillInCells;
                 }
             }
@@ -62,14 +87,17 @@ function runSimulation() {
             inform_option_not_implemented(exercise_selection, element_selection)
         }
     } else if (exercise_selection === "Aritmética") {
-        if (true) {
-
-        } else if (3 < 2) {
-
+        if (element_selection === "Figuras") {
+            console.log(1)
+        } else if (element_selection === "Números") {
+            generate_matrix(matrix_size);
         } else {
+            console.log(3)
             inform_option_not_implemented(exercise_selection, element_selection)
         }
     } else {
+        console.log(4)
         inform_option_not_implemented(exercise_selection, element_selection)
     }
+    console.log(`exercise_selection === ${exercise_selection}, element_selection === ${element_selection})`)
 }
