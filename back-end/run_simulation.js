11542172894677b5ab81d8f4ec01dfd2b9d3f4ec
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2020-04-20 23:49:05 +0200

    ←→ fill_row and assign_random_direction

--- back-end/run_simulation.js
+++ back-end/run_simulation.js
@@ -33,6 +33,23 @@ function assign_n_figures(n) {
     return figure;
 }
 
+function assign_random_direction() {
+    let direction;
+    const direction_selector = Math.random()
+    if (direction_selector < 0.25) {
+        direction = "→"
+    } else if (direction_selector >= 0.25 & direction_selector < 0.5) {
+        direction = "↓"
+    } else if (direction_selector >= 0.5 & direction_selector < 0.75) {
+        direction = "←"
+    } else if (direction_selector >= 0.75 & direction_selector < 1) {
+        direction = "↑"
+    } else {
+        direction = "→"
+    }
+    return direction
+}
+
 function fill_occupied_cell(htmlToFillContentsInMatrices, postHtmlToFillInCells) {
     const element_selection = document.getElementById("element_selection").value;
     if (element_selection === "Matrices") {
@@ -42,13 +59,13 @@ function fill_occupied_cell(htmlToFillContentsInMatrices, postHtmlToFillInCells)
         htmlToFillContentsInMatrices += '<td class="' + color + '"' + postHtmlToFillInCells;
     } else if (element_selection === "Números") {
         const color = assign_n_colors(2);
-        postHtmlToFillInCells = 'style=\
+        postHtmlToFillInCells = ' style=\
         "color:' + color + ';font-size:100px;font-weight:bolder;width:120px;height:120px;"\
         >' + Math.floor(10 * Math.random()) + '</td>';
         htmlToFillContentsInMatrices += '<td ' + postHtmlToFillInCells;
     } else if (element_selection === "Figuras") {
         const figure = assign_n_figures(3);
-        postHtmlToFillInCells = 'style=font-size:100px;font-weight:bolder;width:120px;height:120px;"\
+        postHtmlToFillInCells = ' style=font-size:100px;font-weight:bolder;width:120px;height:120px;"\
         >' + figure + '</td>';
         htmlToFillContentsInMatrices += '<td ' + postHtmlToFillInCells;
     }
@@ -60,44 +77,48 @@ function fill_blank_cell(element_selection) {
         element_selection === "Colores" ||
         element_selection === "Matrices"
     ) {
-        postHtmlToFillInCells = 'style=\
+        postHtmlToFillInCells = ' style=\
             "width:120px;height:120px;border:2px solid #282a2e;"\
             ></td>';
     } else {
-        postHtmlToFillInCells = 'style=\
+        postHtmlToFillInCells = ' style=\
             "width:120px;height:120px;"></td>';
     }
     return postHtmlToFillInCells;
 }
 
+function fill_row(counter, htmlToFillContentsInMatrices, postHtmlToFillInCells) {
+    const elements_number = parseInt(document.getElementById("elements_number").innerText);
+    const p_fill = (1 - (elements_number / 18 /*(matrix_size ^ 2) * 2*/ ));
+    if (Math.random() > p_fill) {
+        htmlToFillContentsInMatrices = fill_occupied_cell(htmlToFillContentsInMatrices, postHtmlToFillInCells)
+        counter += 1
+    } else {
+        postHtmlToFillInCells = fill_blank_cell(element_selection)
+        htmlToFillContentsInMatrices += '<td ' + postHtmlToFillInCells;
+    }
+    return [counter, htmlToFillContentsInMatrices]
+}
+
 function fill_matrix(rows_size, cols_size = rows_size, callback) {
     let counter = 0;
     let htmlToFillContentsInMatrices;
-    const prevHtmlToFillInCells = '<td ';
     const element_selection = document.getElementById("element_selection").value;
     const matrix_size = parseInt(document.getElementById("matrix_size").innerText.substring(0, 1));
     const elements_number = parseInt(document.getElementById("elements_number").innerText);
     let postHtmlToFillInCells;
     if (element_selection === "Matrices") {
-        postHtmlToFillInCells = 'style=\
+        postHtmlToFillInCells = ' style=\
         "width:120px;height:120px;border:2px solid #282a2e;"\
         ></td>';
     }
-    const p_fill = (1 - (elements_number / 18 /*(matrix_size ^ 2) * 2*/ ));
-    console.log(matrix_size + " " + p_fill)
     while (counter < elements_number) {
         counter = 0;
         htmlToFillContentsInMatrices = "";
         for (i = 0; i < rows_size; i++) {
             htmlToFillContentsInMatrices += '<tr>';
             for (j = 0; j < cols_size; j++) {
-                if (Math.random() > p_fill) {
-                    htmlToFillContentsInMatrices = fill_occupied_cell(htmlToFillContentsInMatrices, postHtmlToFillInCells)
-                    counter += 1
-                } else {
-                    postHtmlToFillInCells = fill_blank_cell(element_selection)
-                    htmlToFillContentsInMatrices += prevHtmlToFillInCells + postHtmlToFillInCells;
-                }
+                [counter, htmlToFillContentsInMatrices] = fill_row(counter, htmlToFillContentsInMatrices, postHtmlToFillInCells);
             }
             htmlToFillContentsInMatrices += '</tr>';
         }
@@ -144,28 +165,8 @@ function run_simulation() {
         }
     } else if (exercise_selection === "Habilidad visioespacial") {
         if (element_selection === "Figuras") {
-            let direction;
-            const direction_selector = Math.random()
-            if (direction_selector < 0.25) {
-                direction = "→"
-            } else if (direction_selector >= 0.25 & direction_selector < 0.5) {
-                direction = "↓"
-            } else if (direction_selector >= 0.5 & direction_selector < 0.75) {
-                direction = "←"
-            } else if (direction_selector >= 0.75 & direction_selector < 1) {
-                direction = "↑"
-            } else {
-                direction = "→"
-            }
-            let figure;
-            const figure_selector = Math.random()
-            if (figure_selector < 0.33) {
-                figure = "▲"
-            } else if (figure_selector > 0.66) {
-                figure = "◉"
-            } else {
-                figure = "■"
-            }
+            const direction = assign_random_direction();
+            const figure = assign_n_figures(3);
             document.getElementById("title_placeholder").innerText = figure + " " + direction;
             generate_matrix(matrix_size);
         } else {
@@ -176,5 +177,5 @@ function run_simulation() {
         document.getElementById("title_placeholder").innerText = "";
         inform_option_not_implemented(exercise_selection, element_selection)
     }
-    console.log(`exercise_selection === ${exercise_selection}, element_selection === ${element_selection})`)
+    // console.log(`exercise_selection === ${exercise_selection}, element_selection === ${element_selection})`)
 }
